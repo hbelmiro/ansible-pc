a3ee1370805db57ad860e645b02e76422d29f3f8
hbelmiro <helber.belmiro@gmail.com>
2024-02-09 14:39:19 -0300

    added install pip

--- run.sh
+++ run.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
-ansible-playbook first_playbook.yaml
+set -e
+
+install_pip() {
+    if ! command -v pip &> /dev/null; then
+        echo "pip is not installed. Installing..."
+        if dnf install python3-pip; then
+            echo "pip has been successfully installed."
+        else
+            echo "Failed to install pip. Please install it manually."
+            exit 1
+        fi
+    else
+        echo "pip is already installed."
+    fi
+}
+
+install_ansible() {
+    pip install ansible
+}
+
+main() {
+    install_pip
+    install_ansible
+    ansible-playbook first_playbook.yaml
+}
+
+main
