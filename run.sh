9f6597ebc33e6b416b6fe096e1088846a5c66a53
hbelmiro <helber.belmiro@gmail.com>
2024-02-16 20:11:50 -0300

    Fixed vscode installation
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- run.sh
+++ run.sh
@@ -183,8 +183,7 @@ install_vscode() {
 
         sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
         sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-        dnf check-update
-        sudo dnf install code
+        sudo dnf install code -y
     else
         log "Visual Studio Code is already installed."
     fi
